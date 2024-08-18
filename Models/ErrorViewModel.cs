04e5a4df7aa1a09712d599baf8d364ad4bac5180
yuliya <yuliyakiyanovska@gmail.com>
2024-08-18 23:42:28 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnnouncementMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
