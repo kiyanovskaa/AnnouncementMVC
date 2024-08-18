01a44adb6a372183e581efe4cc49e1461866a27e
yuliya <yuliyakiyanovska@gmail.com>
2024-08-19 01:46:36 +0300

    add editView

--- Models/CreateAnnouncementViewModel.cs
+++ Models/CreateAnnouncementViewModel.cs
@@ -4,6 +4,7 @@ namespace AnnouncementMVC.Models
 {
     public class CreateAnnouncementViewModel
     {
+        public int Id { get; set; } 
         public string Title { get; set; }
         public string Description { get; set; }
     }
